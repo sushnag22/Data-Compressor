25a536ed3a7c00b1aa64ac219e0efb57199d4a93
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-07-05 20:40:22 +0530

    Add main class

---
+++ src/tech/sushnag22/HuffmanCoding.java
@@ -0,0 +1,11 @@
+package tech.sushnag22;
+
+/**
+ *
+ * @author Sushruth Nagaraj
+ */
+public class HuffmanCoding {
+    public static void main(String[] args) {
+        new Form().setVisible(true);
+    }
+}
