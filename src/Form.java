bbca4235e87f878e48e4671a292972e6a52facaa
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-07-10 19:09:37 +0530

    Update form

--- src/Form.java
+++ src/Form.java
@@ -50,7 +50,7 @@ public class Form extends javax.swing.JFrame{
         jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 13)); // NOI18N
-        jLabel1.setText("Data Compression and Decompression using Huffman Coding Technique");
+        jLabel1.setText("Data CompAndDecomp and Decompression using Huffman Coding Technique");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -222,7 +222,7 @@ public class Form extends javax.swing.JFrame{
     private void btn_compressMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_compressMouseClicked
         String input = txt_input.getText();
         String hcode = "", ecode = "", dcode = "";
-        Compression cp = new Compression();
+        CompAndDecomp cp = new CompAndDecomp();
         hcode = cp.buildHuffmanTree(input);
         ecode = cp.getEncoded(input);
         dcode = cp.getDecoded();
